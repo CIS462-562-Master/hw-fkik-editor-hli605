1e88dd7c8d4d47ab1dc2bdd237d9221940898a58
hli605 <hli605@gatech.edu>
2020-10-26 10:45:34 +0800

    Part3 Completed

--- src/animation/aActor.cpp
+++ src/animation/aActor.cpp
@@ -10,7 +10,7 @@
 *
 ****************************************************************/
 
-AActor::AActor() 
+AActor::AActor()
 {
 	m_pInternalSkeleton = new ASkeleton();
 	m_pSkeleton = m_pInternalSkeleton;
@@ -48,24 +48,24 @@ AActor& AActor::operator = (const AActor& actor)
 
 AActor::~AActor()
 {
-	 delete m_IKController;
-	 delete m_BVHController;
-	 delete m_pInternalSkeleton;
+	delete m_IKController;
+	delete m_BVHController;
+	delete m_pInternalSkeleton;
 
 }
 
 void AActor::clear()
 {
 	// looks like it is clearing more times than the number of actors.  as a result, m_pSkeleton is not defined for last case.
-	m_pSkeleton->clear();  
+	m_pSkeleton->clear();
 
 	// code to update additional Actor data goes here
 }
 
 void AActor::update()
 {
-	if (!m_pSkeleton->getRootNode() )
-		 return; // Nothing loaded
+	if (!m_pSkeleton->getRootNode())
+		return; // Nothing loaded
 	else m_pSkeleton->update();
 
 	// code to update additional Actor data goes here
@@ -105,6 +105,19 @@ void AActor::updateGuideJoint(vec3 guideTargetPos)
 	// 1.	Set the global position of the guide joint to the global position of the root joint
 	// 2.	Set the y component of the guide position to 0
 	// 3.	Set the global rotation of the guide joint towards the guideTarget
+	AJoint* rootNode = m_pSkeleton->getRootNode();
+	vec3 rootGlobalPos = m_Guide.getGlobalTranslation() + m_Guide.getGlobalRotation() * rootNode->getGlobalTranslation();
+	rootGlobalPos[1] = 0;
+	m_Guide.setGlobalTranslation(rootGlobalPos);
+
+	guideTargetPos[1] = 0;
+	vec3 u1 = vec3(0, 0, 1);
+	vec3 u2 = (guideTargetPos - m_Guide.getGlobalTranslation()).Normalize();
+	vec3 axis = u1 ^ u2;
+	double angle = acos(u1 * u2);
+	mat3 newRot;
+	newRot.FromAxisAngle(axis, angle);
+	m_Guide.setGlobalRotation(newRot);
 }
 
 void AActor::solveFootIK(float leftHeight, float rightHeight, bool rotateLeft, bool rotateRight, vec3 leftNormal, vec3 rightNormal)
@@ -117,21 +130,51 @@ void AActor::solveFootIK(float leftHeight, float rightHeight, bool rotateLeft, b
 	// The normal and the height given are in the world space
 
 	// 1.	Update the local translation of the root based on the left height and the right height
-
+	vec3 rootPos = m_pSkeleton->getRootNode()->getGlobalTranslation();
+	rootPos[1] += (leftHeight + rightHeight) / 2;
+	m_pSkeleton->getRootNode()->setLocalTranslation(rootPos);
+	
 	m_pSkeleton->update();
 
 	// 2.	Update the character with Limb-based IK 
-	
+
 	// Rotate Foot
 	if (rotateLeft)
 	{
 		// Update the local orientation of the left foot based on the left normal
-		;
+		ATarget m_target;
+		vec3 leftFootGlobal = leftFoot->getGlobalTranslation();
+		leftFootGlobal[1] = leftHeight;
+		m_target.setGlobalTranslation(leftFootGlobal);
+
+		vec3 leftNormalLocal = (leftFoot->getLocal2Global().m_rotation.Inverse() * leftNormal).Normalize();
+		vec3 u = vec3(0, 1, 0);
+		vec3 axis = u ^ leftNormalLocal;
+		double angle = acos(u * leftNormalLocal.Normalize());
+		mat3 newRot;
+		newRot.FromAxisAngle(axis, angle);
+		leftFoot->setLocalRotation(newRot);
+
+		m_IKController->IKSolver_Limb(leftFoot->getID(), m_target);
 	}
 	if (rotateRight)
 	{
 		// Update the local orientation of the right foot based on the right normal
-		;
+		ATarget m_target;
+		vec3 rightFootGlobal = rightFoot->getGlobalTranslation();
+		rightFootGlobal[1] = rightHeight;
+		m_target.setGlobalTranslation(rightFootGlobal);
+		
+
+		vec3 rightNormalLocal = (rightFoot->getLocal2Global().m_rotation.Inverse() * rightNormal).Normalize();
+		vec3 u = vec3(0, 1, 0);
+		vec3 axis = u ^ rightNormalLocal;
+		double angle = acos(u * rightNormalLocal);
+		mat3 newRot;
+		newRot.FromAxisAngle(axis, angle);
+		rightFoot->setLocalRotation(newRot);
+
+		m_IKController->IKSolver_Limb(rightFoot->getID(), m_target);
 	}
 	m_pSkeleton->update();
 }
